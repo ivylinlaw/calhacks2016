2e14f68b698a12b2754cfb31e87f64b937ccd9de
weiwupan <weiwupan@usc.edu>
2016-11-12 14:08:10 -0800

    add source file from example

---
+++ CalHacks/app/src/main/java/com/example/ivylinlaw/calhacks/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.example.ivylinlaw.calhacks;
+
+public class ApplicationData {
+	public static final String CLIENT_ID = "c9cfd4a7565a48bfa5cc846a92e54bae";
+	public static final String CLIENT_SECRET = "fda9b60272804f5d8824eb9b320dcd90";
+	public static final String CALLBACK_URL = "http://github.com/oscarpang";
+}
